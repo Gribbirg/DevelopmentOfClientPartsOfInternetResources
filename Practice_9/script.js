d23ddc4abc94022d6aa5ffb37a3c13649072557f
Alexander Gribkov <gribkovalexander@gmail.com>
2023-11-02 18:38:36 +0300

    Practice 9 ex 1, 2 finish

--- Practice_9/script.js
+++ Practice_9/script.js
@@ -1,16 +1,34 @@
 "use strict"
 
+function authPrompt() {
+    let name = prompt("Кто ты?",);
 
-document.getElementById("reg_form").onsubmit = function(){
-    if (document.getElementById("reg_ans").value === "Да")
+    if (!name ?? name === "")
+        alert("Отменено")
+    else if (name === "Админ")
+        passwordPrompt()
+    else
+        alert("Я вас не знаю")
+}
+
+function passwordPrompt() {
+    let pass = prompt("Введите пароль");
+
+    if (!pass ?? pass === "")
+        alert("Отменено");
+    else if (pass === "Я главный")
+        alert("Здравствуйте!");
+    else
+        alert("Неверный пароль");
+}
+
+
+document.getElementById("reg_form").onsubmit = function () {
+    let enter = document.getElementById("reg_ans").value;
+    if (enter === "Да")
         document.getElementById("reg_ans_label").innerHTML = "Круто!";
+    else if (enter === "Админ")
+        authPrompt();
     else
         document.getElementById("reg_ans_label").innerHTML = "Попробуй ещё раз ";
 }
-
-// let promtRes = prompt('Кто ты?')
-
-// if (promtRes === 'Админ')
-//     alert("Ты легенда!")
-// else
-//     alert("Я легенда!")
