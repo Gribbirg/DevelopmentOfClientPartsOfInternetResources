ee2362d315357f2794cf5402dcb297217ff3a1b0
Alexander Gribkov <gribkovalexander@gmail.com>
2023-11-02 21:35:55 +0300

    Practice 9 finish

--- Practice_9/script.js
+++ Practice_9/script.js
@@ -32,3 +32,49 @@ document.getElementById("reg_form").onsubmit = function () {
     else
         document.getElementById("reg_ans_label").innerHTML = "Попробуй ещё раз ";
 }
+
+function setColorForButton(button) {
+    if (button.style.getPropertyValue("--color") === "black") {
+        button.style.backgroundColor = "#ffdad6";
+        button.style.setProperty("--color", "#ba1a1a")
+    } else {
+        button.style.backgroundColor = "white";
+        button.style.setProperty("--color", "black")
+    }
+}
+
+function spawnHeart(e) {
+    let heart = document.createElement("a");
+
+    heart.style.position = 'absolute';
+    heart.style.left = e.pageX.toString() + "px";
+    heart.style.top = e.pageY.toString() + "px";
+    heart.innerHTML = "❤";
+    heart.style.zIndex = "5";
+    heart.style.color = "#03c700"
+
+    document.body.appendChild(heart);
+
+}
+
+function checkHeartsNeed() {
+    if (!spawnHearts) {
+        spawnHearts = true;
+    } else {
+        window.onmousemove = null;
+        spawnHearts = false;
+        window.onclick = null;
+    }
+}
+
+let spawnHearts = false;
+
+for (let button of document.querySelectorAll('#pic_div > div > button')) {
+    button.style.setProperty("--color", "black")
+    button.onclick = function () {
+        setColorForButton(button)
+        window.onmousemove = e => spawnHeart(e);
+        window.onclick = checkHeartsNeed;
+    }
+}
+
