012781208afe8c7ef2f195a2a3250a588224b7bd
Alexander Gribkov <gribkovalexander@gmail.com>
2023-11-15 15:35:55 +0300

    Practice 11 json work v2

--- Practice_11/pages/products/script.js
+++ Practice_11/pages/products/script.js
@@ -5,8 +5,10 @@ function getCategory() {
 }
 
 function setCategoryName(name) {
-    document.title = name;
-    document.body.querySelector("#products_section .part_name").innerHTML = name;
+    if (name) {
+        document.title = name;
+        document.body.querySelector("#products_section .part_name").innerHTML = name;
+    }
 }
 
 function createProductDiv(product) {
@@ -17,25 +19,14 @@ function GetContent(category) {
     let content = data.find(function (item) {
         return item.id === category;
     });
+    setCategoryName(content.name);
     alert(content.name);
-
 }
 
 let category = getCategory();
 
 switch (category) {
-    case "chemistry":
-        setCategoryName("Масло и автохимия");
-        break;
-    case "tiers":
-        setCategoryName("Шины и диски");
-        break;
-    case "electronics":
-        setCategoryName("Автоэлектроника");
-        break;
-    case "other":
-        setCategoryName("Остальное");
-        break;
+
 }
 
 import data from '../../products_list/products.json' assert {type: 'json'};
