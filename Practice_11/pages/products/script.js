78ad1a33cdeb807977cca26dbe75601d31176c9c
Alexander Gribkov <gribkovalexander@gmail.com>
2023-11-15 23:37:28 +0300

    Practice 11 json show work

--- Practice_11/pages/products/script.js
+++ Practice_11/pages/products/script.js
@@ -11,14 +11,66 @@ function setCategoryName(name) {
     }
 }
 
+function betterNumber(num) {
+    let ans = (String)(num % 1000);
+    while (num > 0) {
+        num = Math.floor(num / 1000);
+        ans = (num % 1000) + " " + ans;
+    }
+}
+
 function createProductDiv(product) {
     let div = document.createElement("div");
-
     div.style.width = "100%";
-    div.style.height = "100px";
-    div.style.background = "#000000"
-
+    div.style.height = "200px";
+    div.style.background = "#fcfdf6";
+    // div.style.border = "#1a1c18 1px solid";
+    div.style.borderRadius = "10px";
+    div.style.boxShadow = "1px 1px 3px 2px grey";
+    div.style.display = "grid";
+    div.style.gridTemplate = "repeat(3, fit-content(100%)) / fit-content(100%) 1fr fit-content(100%)";
+    div.style.gridTemplateAreas = '"pic head cost" "pic desc cost" "pic button button"'
+    // div.style.justifyContent = "space-between";
+    // div.style.alignItems = "center";
     document.getElementById("products_div").appendChild(div);
+
+    let img = document.createElement("img");
+    img.src = "../../images/products/" + product["img"];
+    img.style.height = "180px";
+    img.style.margin = "10px";
+    img.style.gridArea = "pic";
+    div.appendChild(img);
+
+    // let textDiv = document.createElement("div");
+    // textDiv.style.height = "100%";
+    // textDiv.style.width = "100%";
+    // textDiv.style.margin = "10px"
+    // div.appendChild(textDiv);
+
+    let head = document.createElement("h3");
+    head.innerHTML = product.name;
+    head.style.gridArea = "head";
+
+    div.appendChild(head);
+
+    let cost = document.createElement("p");
+    cost.innerHTML = product["cost"].toLocaleString() + " ₽";
+    cost.style.lineBreak = "none";
+    cost.style.whiteSpace = "nowrap";
+    cost.style.margin = "10px";
+    cost.style.fontSize = "2rem";
+    cost.style.gridArea = "cost";
+    div.appendChild(cost);
+
+    let desc = document.createElement("p");
+    desc.innerHTML = product.description;
+    desc.style.gridArea = "desc";
+    div.appendChild(desc);
+
+    let button = document.createElement("button");
+    button.style.gridArea = "button";
+    button.style.margin = "10px";
+    div.appendChild(button);
 }
 
 function GetContent(category) {
@@ -38,4 +90,5 @@ switch (category) {
 }
 
 import data from '../../products_list/products.json' assert {type: 'json'};
+
 GetContent(category)
