a451ec6b86a4f8530e842b593025e0f307a821b6
Alexander Gribkov <gribkovalexander@gmail.com>
2023-12-08 16:42:07 +0300

    Practice 14 finish

--- Practice_14/main/script.js
+++ Practice_14/main/script.js
@@ -7,3 +7,37 @@ for (let p of document.querySelectorAll("#blur_card_div > div > p, #anim_line_di
 function truncate(str, maxlength) {
     return (str.length <= maxlength) ? str : str.slice(0, maxlength - 3) + "...";
 }
+
+let slider_position = document.getElementById("slider_position_div")
+
+slider_position.onmousedown = function (event) {
+    event.preventDefault();
+
+    let shiftX = event.clientX - slider_position.getBoundingClientRect().left;
+
+    function move(event) {
+        let position = event.clientX - shiftX - document.getElementById("slider_div").getBoundingClientRect().left;
+
+        if (position < 0) {
+            position = 0;
+        }
+        if (position > 300 - slider_position.offsetWidth) {
+            position = 300 - slider_position.offsetWidth;
+        }
+
+        slider_position.style.left = position + 'px';
+    }
+
+    function stop() {
+        document.removeEventListener('mousemove', move);
+        document.removeEventListener('mouseup', stop);
+    }
+
+    document.addEventListener('mousemove', move);
+    document.addEventListener('mouseup', stop);
+}
+
+
+slider_position.ondragstart = function() {
+    return false;
+};
